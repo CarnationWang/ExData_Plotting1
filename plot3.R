d63a885afc56d4159245b42d895272551d5366bf
JudyWang <neverdreamaway@qq.com>
2015-01-10 11:01:28 +0800

    update plot3
    there is something wrong with the presentation

--- plot3.R
+++ plot3.R
@@ -25,7 +25,7 @@ with(DT2,
 
 legend("topright",lty=1,
 	  col=c("black","red","blue"),
-	  legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+	  legend=c("Sub_metering_1  ","Sub_metering_2  ","Sub_metering_3  "))
 
 #save as png
 dev.copy(png, file="plot3.png") 
